277b36fd4f972f2cd4dad8ab33bf1c6e91c400db
Jan van Mansum <janvanmansum@gmail.com>
2025-02-11 10:13:05 +0100

    typo

--- previewers/v1.4-DANS-PATCH-1/js/video.js
+++ previewers/v1.4-DANS-PATCH-1/js/video.js
@@ -101,7 +101,7 @@ function appendVideoElements(fileUrl, videoId, files, siteUrl, userLanguages, ap
     }
 
     const videoElement = $("<video/>")
-        .attr("corssorigin", "anonymous") // required for getting subtitles from object storage
+        .attr("crossorigin", "anonymous") // required for getting subtitles from object storage
         .prop("controls", true)
         .append($('<source/>').attr("src", fileUrl));
 
